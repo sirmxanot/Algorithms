f9548063b151cd2e232a91fe65ab913ffb95df1c
Andrew Frank <andrewpaulfrank@gmail.com>
2013-03-17 18:28:58 -0300

    add two_sum and median_maintenance

---
+++ Sorting/Ruby/two_sum.rb
@@ -0,0 +1,26 @@
+def two_sum(interval_low = 2500, interval_high = 4000, file = "HashInt.txt")
+  @universe = Hash.new
+  @solutions = 0
+
+  #read in raw graph
+  File.open(file, "r") do |file_handle|
+    file_handle.each_line do |line|
+      number = line.to_i
+      @universe[number] = number unless @universe.has_key?(number)
+    end
+  end 
+
+  (interval_low..interval_high).each do |goal|
+    @solutions += 1 if satisfied?(goal)
+  end
+
+  @solutions
+end
+
+def satisfied?(goal)
+  true if @universe.keys.detect { |x| solution_exists?(goal, x) }
+end
+
+def solution_exists?(goal, x)
+  @universe.has_key?(goal - x) if x != goal - x
+end
