59cb04d80272c1a08b5b9a3fa03c506f88c881bc
Andrew Frank <andrewpaulfrank@gmail.com>
2013-10-09 19:06:52 -0700

    add reverse.js

--- javascript/LinkedList/reverse.js
+++ javascript/LinkedList/reverse.js
@@ -1,11 +1,9 @@
 // Non-destructively reverses a linked list
-var LinkedList = require('./LinkedList');
+var LinkedList = require('./LinkedList.js');
 
-function TestData() {
-  this.a = new LinkedList([1,2,3,4,5,6]);
-  this.b = new LinkedList(["a",1,2,-3,54.5]);
-  this.c = new LinkedList([0,1,2,2,1,0]);
-  this.d = new LinkedList([]);
-}
 
-console.log(new TestData());
+  console.log(new TestData());
+
+
+  
+
