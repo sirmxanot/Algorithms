6a3fb210f989c1ebe4fbba6024648be35d31f617
Andrew Frank <andrewpaulfrank@gmail.com>
2013-10-20 15:31:02 -0700

    add Peon Numbers to scala

--- scala/LinkedList/LinkedList.scala
+++ scala/LinkedList/LinkedList.scala
@@ -24,3 +24,9 @@ object nth {
     else nth(n-1, xs.tail)
   }
 }
+
+object List {
+  // List(1,2) = List.apply(1,2)
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+  def apply[T]() = new Nil
+}
