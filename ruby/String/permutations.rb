4113bcb76a23e6112d065472618f247debb46fe6
Andrew Frank <andrewpaulfrank@gmail.com>
2014-03-16 12:42:21 -0700

    complete and test String.permutations.rb

--- ruby/String/permutations.rb
+++ ruby/String/permutations.rb
@@ -1,33 +1,26 @@
 class String
   def find_permutations_easy
-    self.chars.permutation.map &:join
+    self.chars.to_a.permutation.map &:join
   end
 
-  #heap's algorithm
   def find_permutations
     permutations = []
 
     if self.size == 1
-      return permutations
+      return [self]
     else
       self.chars.each do |char|
-        puts "char #{char}"
-        remaining_chars = self.delete char
-        puts "remaining_chars #{remaining_chars}"
+        remaining_chars = self.clone.delete char
         
         sub_permutations = remaining_chars.find_permutations
 
-        puts "sub_permutations #{sub_permutations}"
-
-        permutations << sub_permutations
-       
-        # .each do |sub_perm|
-        #   puts "sub_perm #{sub_perm}"
-        #   permutations << char + sub_perm
-        #   puts "permutations #{permutations}"
-        # end
+        sub_permutations.each do |sub_perm|
+          permutations << char + sub_perm
+        end
       end
     end
+
+    return permutations
   end
 end
 
@@ -37,6 +30,6 @@ def test
     "cadb", "cabd", "dacb", "adcb", "acdb", "acbd", "dabc", "adbc", "abdc", 
     "abcd"].sort 
 
-  #puts "abcd".find_permutations_easy.sort == permutations 
-  puts "abcd".find_permutations.sort      #== permutations 
+  puts "abcd".find_permutations_easy.sort == permutations 
+  puts "abcd".find_permutations.sort      == permutations
 end
