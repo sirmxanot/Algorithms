6c604fb3965bc18e696f1f4ac3c9741a9112c0b6
Frank <affrank@3c15c2d0e54c.ant.amazon.com>
2014-08-28 15:30:15 -0700

    add comments to clarify DistributeEvenly.rb

--- ruby/SelectionAlgorithms/DistributeEvenly.rb
+++ ruby/SelectionAlgorithms/DistributeEvenly.rb
@@ -1,23 +1,33 @@
 #given an array of i distinct objects and a number of buckets j, returns an array of arrays where there
 # are j elements in the highest level array and each sub-array holds (i/j) +- 1 objects and every object is
-# in exactly one sub-array
+# in exactly one sub-array. it is deterministic (returns the exact same results for the same inputs every time)
 
 class Array
   def distribute_across bucket_number
+    #clone original array so that it can be reused
+    #de dupe it and sort it so the output is clearer
     original_array = self.clone.uniq.sort
     parent = Array.new
 
+    #did this instead of Array.new(bucket_number, Array.new) so that all the
+    # child arrays dont point to the same object
     bucket_number.times do |child|
       parent << Array.new
     end
     
+    #keep cycling through the child arrays adding one element from original array at a time until there are no more elements in origianl_array
     until original_array.empty?
+      #go through each child array and add the first value in the original array to it
       parent.each do |child|
+        #remove first element of original array and assign it to new_element
         new_element = original_array.shift
+
+        #put this element at the end of the child array and avoid putting in nil values
         child << new_element unless new_element.nil?
       end
     end
 
+    #sort child arrays within the parent array for clarity
     return parent.sort
   end
 end
@@ -42,12 +52,10 @@ def bucket_sizes_even output
 end
 
 def test
-  #test_data = [[42],[1,4],[1,2,3,4,5]] #,[4,6,2],[1,5,2,5,6,4,2,6],(1..11).to_a]
-  test_data = [[1,2,2,2,4,2,3,8]]
+  test_data = [[42],[1,4],[1,2,3,4,5],[4,6,2],[1,5,2,5,6,4,2,6],(1..11).to_a]
+  
   test_data.each do |datum|
     (1..datum.uniq.length+1).each do |number_of_buckets|
-      puts "Datum is #{datum}"
-      puts "Number of Buckets #{number_of_buckets}"
       puts datum.distribute_across(number_of_buckets).to_s
       puts size_of_parent_array number_of_buckets, datum.distribute_across(number_of_buckets)
       puts all_elements_distributed datum, datum.distribute_across(number_of_buckets)
